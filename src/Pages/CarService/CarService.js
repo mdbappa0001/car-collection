db4204701a1ab68dd88814c1edc3d087d1252bb2
mdbappa0001 <abutanif366@gmail.com>
2022-05-10 22:32:04 +0600

    readme file updated

--- src/Pages/CarService/CarService.js
+++ src/Pages/CarService/CarService.js
@@ -8,7 +8,7 @@ const CarService = () => {
             <div>
             <img className='carImage' src={servicingCenter} alt="" />
             </div>
-            <div className='description'>
+            <div className='description1'>
                 <h1 className='carHeading'>Welcome To Our Servicing Center</h1>
                 <p className='carDescription'>Enjoy Technology That Makes You Feel More Connected, Confident, And Excited. Drive A Nissan Car So Advanced, You'll Feel Like You're Driving The Future. Exhilarating Power. Automated Breaking. Advanced Safety. Leading Technology. Remote Start. Dynamic Design. Models: Rogue, Altima, Sentra.</p>
             </div>
